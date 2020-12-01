de2e9386e2e3347bcfe9e83e422336a021265a22
rkinwork <rkinwork@gmail.com>
2020-12-01 10:54:56 +0300

    Replace BS4 find methods with select one

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import re
 from pathlib import Path
 from typing import NamedTuple, Tuple
 from urllib.parse import urljoin
-from itertools import chain
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,6 +13,7 @@ PARSE_BOOK_METADATA_URL_TEMPL = urljoin(SITE_HOST, 'b{}/')
 BOOKS_ROOT = Path('./books')
 MEDIA_EXISTS_STATUS_CODE = 200
 FILENAME_PATTERN = re.compile(r'filename="(.+)"')
+BOOK_ID_PATTERN = re.compile(urljoin(SITE_HOST, r'b(\d+)'))
 BOOK_NAME_TEMPL = '{book_id}. {book_name}.txt'
 AUTHOR_BOOK_NAME_SEPARATOR = ' - '
 
@@ -46,46 +46,47 @@ def download_book(book_url):
                 f.write(chunk)
 
 
-def download_books():
-    for book_id in range(1, 11):
-        book_metadata = get_book_metadata(book_id)
-        if book_metadata.title is None:
-            continue
-        # print(book_metadata)
-        download_image(book_metadata.img_url)
-        download_txt(DOWNLOAD_BOOK_URL_TEMPL.format(book_id),
-                     BOOK_NAME_TEMPL.format(book_id=book_id, book_name=book_metadata.title),
-                     BOOKS_ROOT)
+def get_book_by_url(book_url) -> [dict]:
+    book_metadata = get_book_metadata(book_url)
+    if book_metadata.title is None:
+        return None
+    book_id = BOOK_ID_PATTERN.search(book_url).group(1)
+    book_path = download_txt(url=DOWNLOAD_BOOK_URL_TEMPL.format(book_id),
+                             file_name=BOOK_NAME_TEMPL.format(book_id=book_id,
+                                                              book_name=book_metadata.title),
+                             folder=BOOKS_ROOT)
+    if book_path is None:
+        return None
+
+    return {'author': book_metadata.author,
+            'title': book_metadata.title,
+            'comments': book_metadata.comments,
+            'genres': book_metadata.genres,
+            'img_src': download_image(url=book_metadata.img_url),
+            'book_path': book_path}
 
 
 def extract_comments(soup: BeautifulSoup) -> Tuple[str, ...]:
-    content_block = soup.find(id='content')
-    div_texts = content_block.find_all('div', class_='texts')
-    blocks_with_text = [text_block.find('span', class_='black').contents
-                        for text_block in div_texts if text_block.find('span', class_='black')]
-
-    return tuple([''.join(block_text) for block_text in blocks_with_text])
+    blocks_with_text = soup.select("#content .texts .black")
+    return tuple([''.join(block_text.get_text()) for block_text in blocks_with_text])
 
 
 def extract_genres(soup: BeautifulSoup) -> Tuple[str, ...]:
-    span_d_book = soup.find_all('span', class_='d_book')
-    a_genres = chain.from_iterable(
-        span_book_group.find_all('a', href=lambda x: x.startswith('/l')) for span_book_group in span_d_book)
+    a_genres = soup.select('span.d_book a', href=lambda x: x.startswith('/l'))
     return tuple([a_genre.string for a_genre in a_genres])
 
 
-def get_book_metadata(book_id: int) -> BookMetadata:
-    with requests.get(PARSE_BOOK_METADATA_URL_TEMPL.format(book_id), verify=False) as r:
+def get_book_metadata(book_url: str) -> BookMetadata:
+    with requests.get(book_url, verify=False) as r:
         if r.status_code != MEDIA_EXISTS_STATUS_CODE:
             return BookMetadata()
         response_text = r.text
     soup = BeautifulSoup(response_text, 'lxml')
-    res = soup.select('div.bookimage a')
-    if not res or not res[0]['title']:
+    book_image_tag = soup.select_one('.bookimage a')
+    if not book_image_tag or not book_image_tag['title']:
         return BookMetadata()
-    book_image_tag = res[0]
     author_and_title = book_image_tag['title']
-    image_tag = book_image_tag.find('img')
+    image_tag = book_image_tag.select_one('img')
     if not image_tag or not image_tag['src']:
         img_url = None
     else:
@@ -94,7 +95,7 @@ def get_book_metadata(book_id: int) -> BookMetadata:
     comments = extract_comments(soup)
     genres = extract_genres(soup)
 
-    author, title = [el.strip() for el in author_and_title.split(AUTHOR_BOOK_NAME_SEPARATOR)]
+    author, title = [el.strip() for el in author_and_title.split(AUTHOR_BOOK_NAME_SEPARATOR, 1)]
     return BookMetadata(author=author,
                         title=title,
                         img_url=img_url,
@@ -102,7 +103,7 @@ def get_book_metadata(book_id: int) -> BookMetadata:
                         genres=genres)
 
 
-def download_txt(url, file_name, folder='books/') -> [str]:
+def download_txt(url: str, file_name: str, folder: str = 'books/') -> [str]:
     """Функция для скачивания текстовых файлов.
     Args:
         url (str): Cсылка на текст, который хочется скачать.
@@ -151,7 +152,3 @@ def download_image(url, folder='images/') -> [str]:
                 f.write(chunk)
 
         return image_file.as_posix()
-
-
-if __name__ == '__main__':
-    download_books()
---
+++ parse_tululu_category.py
@@ -0,0 +1,53 @@
+from urllib.parse import urljoin
+import json
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+from main import SITE_HOST, MEDIA_EXISTS_STATUS_CODE, get_book_by_url
+
+FANTASTIC_CATEGORY_URL = urljoin(SITE_HOST, '/{category}')
+FANTASTIC_CATEGORY_CODE = 'l55/'
+PAGES_TO_PROCESS_IN_CATEGORY = 1
+FANTASTIC_LIB_JSON = Path('fantastic_lib.json')
+
+
+def extract_fantastic_book_link(table_book: Tag) -> [str]:
+    try:
+        a_tag = table_book.select_one('.bookimage a')
+    except AttributeError:
+        return None
+
+    return urljoin(SITE_HOST, a_tag['href'])
+
+
+def process_category_page(category_code: str, page: str = "1"):
+    category_url = FANTASTIC_CATEGORY_URL.format(category=category_code)
+    url_to_process = urljoin(category_url, page)
+    with requests.get(url_to_process, verify=False) as r:
+        if r.status_code != MEDIA_EXISTS_STATUS_CODE:
+            return None
+        response_text = r.text
+    soup = BeautifulSoup(response_text, 'lxml')
+    table_books = soup.select('table.d_book')
+    # for table_book in table_books[:4]:
+    #     yield extract_fantastic_book_link(table_book)
+
+    return filter(None, (extract_fantastic_book_link(table_book) for table_book in table_books))
+
+
+def extract_href_from_category(category_code):
+    for page in range(1, PAGES_TO_PROCESS_IN_CATEGORY + 1):
+        yield from process_category_page(category_code=category_code, page=str(page))
+
+
+def main():
+    data_to_save = filter(None, (get_book_by_url(book_url) for book_url in
+                                 extract_href_from_category(FANTASTIC_CATEGORY_CODE)))
+    with FANTASTIC_LIB_JSON.open(mode='w', encoding='utf-8') as f:
+        json.dump(list(data_to_save), f, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    main()
