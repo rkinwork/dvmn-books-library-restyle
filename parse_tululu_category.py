47e5a5d4782e5bf2b1e6638e61908df9dc37734d
rkinwork <rkinwork@gmail.com>
2020-12-01 14:57:20 +0300

    Add start_page and end_page arguments

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,3 +1,4 @@
+import argparse
 from urllib.parse import urljoin
 import json
 from pathlib import Path
@@ -9,7 +10,7 @@ from main import SITE_HOST, MEDIA_EXISTS_STATUS_CODE, get_book_by_url
 
 FANTASTIC_CATEGORY_URL = urljoin(SITE_HOST, '/{category}')
 FANTASTIC_CATEGORY_CODE = 'l55/'
-PAGES_TO_PROCESS_IN_CATEGORY = 1
+MAX_PAGES_TO_PROCESS_IN_CATEGORY = 701
 FANTASTIC_LIB_JSON = Path('fantastic_lib.json')
 
 
@@ -37,14 +38,25 @@ def process_category_page(category_code: str, page: str = "1"):
     return filter(None, (extract_fantastic_book_link(table_book) for table_book in table_books))
 
 
-def extract_href_from_category(category_code):
-    for page in range(1, PAGES_TO_PROCESS_IN_CATEGORY + 1):
+def extract_href_from_category(category_code: str,
+                               start_page: int = 1,
+                               end_page: int = MAX_PAGES_TO_PROCESS_IN_CATEGORY + 1):
+    for page in range(start_page, end_page):
         yield from process_category_page(category_code=category_code, page=str(page))
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Download fantastic books from tululu website'
+    )
+    parser.add_argument('--start_page', type=int, default=1, help='Page in category to start parse books')
+    parser.add_argument('--end_page', type=int, default=MAX_PAGES_TO_PROCESS_IN_CATEGORY+1,
+                        help='Page in category to start parse books')
+    args = parser.parse_args()
     data_to_save = filter(None, (get_book_by_url(book_url) for book_url in
-                                 extract_href_from_category(FANTASTIC_CATEGORY_CODE)))
+                                 extract_href_from_category(FANTASTIC_CATEGORY_CODE,
+                                                            start_page=args.start_page,
+                                                            end_page=args.end_page)))
     with FANTASTIC_LIB_JSON.open(mode='w', encoding='utf-8') as f:
         json.dump(list(data_to_save), f, ensure_ascii=False, indent=2)
 
